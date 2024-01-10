5ebca495e964d631952de4dce134d3415c7eca62
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-10 01:21:58 +0100

    read update and delete of products

--- amazon-clone/api/src/product/product.controller.ts
+++ amazon-clone/api/src/product/product.controller.ts
@@ -1,17 +1,42 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { ProductService } from "./product.service";
 import { ProductDocument } from "./product.schema";
 
 @Controller('product')
 export class ProductController {
-  constructor(private productService:ProductService){}
+  constructor(private productService: ProductService) {}
 
   @Post()
   createProduct(
-    @Body('name') name:string,
-    @Body('price') price:number,
-    @Body('description') description: string
-  ): Promise<ProductDocument>{
-    return this.productService.create(name,price,description)
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Promise<ProductDocument> {
+    return this.productService.create(name, price, description);
+  }
+
+  @Get()
+  findAllProducts(): Promise<ProductDocument[]> {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  findProduct(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.find(id);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ): Promise<ProductDocument> {
+    return this.productService.update(id, name, price, description);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.delete(id);
   }
 }
