c921e08235a5b55e48c7790b1d61138c855b7b87
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-15 16:40:10 +0100

    basic configuration

---
+++ amazon-clone/amazon_ui/src/features/auth/authSlice.ts
@@ -0,0 +1,14 @@
+
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+
+}
+
+export const authReducer = {}
+
+export const authSlice = createSlice({
+  name:'auth',
+  initialState,
+  reducers:{}
+})
