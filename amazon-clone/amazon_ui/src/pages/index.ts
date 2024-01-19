d413c9b5719017b65cb66129aea8b723dfb1db18
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-01-19 03:26:13 +0100

    cart page

--- amazon-clone/amazon_ui/src/pages/index.ts
+++ amazon-clone/amazon_ui/src/pages/index.ts
@@ -2,3 +2,4 @@
 export {default as Register} from "./Register"
 export {default as HomePage} from "./HomePage"
 export {default as SigninPage} from "./SigninPage"
+export {default as CartPage} from "./CartPage"
