023df7c60cdd2b9761ab2f10dc710c4dab741749
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-01-07 04:38:17 +0100

    setting up the server configuration

---
+++ messenger_clone/api/apps/auth/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
