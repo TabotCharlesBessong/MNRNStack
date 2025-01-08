7ccd6c55ed90a8b54475054c614bc66f5a3e8628
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-01-08 06:10:19 +0100

    updating our dockerization and configured typeorm

--- messenger_clone/api/apps/auth/src/auth.module.ts
+++ messenger_clone/api/apps/auth/src/auth.module.ts
@@ -1,14 +1,25 @@
-import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Inject, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true,
       envFilePath: './.env',
-    })
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService:ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('POSTGRES_URL'),
+        autoLoadEntities:true,
+        synchronize:true
+      }),
+      inject: [ConfigService]
+    }) 
   ],
   controllers: [AuthController],
   providers: [AuthService],
